9d5fb592f626c6fd9df7a38ce6740982b5172cce
Krish Gupta <krishguptadev@outlook.com>
2022-01-25 14:19:39 +0530

    feat: add redirects for the comm

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  redirects: async () {
+    return [
+      {
+        source: '/chat',
+        destination: 'https://discord.gg/44HdXvA4wX',
+        permanent: true
+      },
+      {
+        source: '/coc',
+        destination: 'https://github.com/InnateComm/.github/blob/main/CODE_OF_CONDUCT.md',
+        permanent: true
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
